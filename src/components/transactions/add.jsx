63725392e889bb384116c0652f415380ab4f35ed
Andrey Savka <a_sawka@yahoo.com>
2016-05-22 00:40:11 +0300

    Add transaction form

--- src/components/transactions/add.jsx
+++ src/components/transactions/add.jsx
@@ -1,19 +1,76 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import Header from '../header';
+import FieldContainer from '../common/field_container';
 
 require('../../stylesheets/transactions/add.scss');
 
-export default class AddTransaction extends Component {
+class AddTransaction extends Component {
+
+  onSubmit() {
+
+  }
 
   render() {
+
+    const { fields: { amount, bankId }, handleSubmit } = this.props;
+
     return (
       <div>
         <Header />
-        <div className="add_transaction content">
-          Add Transaction ...
+        <div className="add-transaction content">
+
+          <form className="add-transaction-form" onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+
+            <h3 className="page-head">Add Transaction</h3>
+
+            <FieldContainer label="Amount" field={ amount }>
+              <input className="field-input" type="text" { ...amount } />
+            </FieldContainer>
+
+            <FieldContainer label="Bank" field={ bankId }>
+              <select className="field-input" type="text" { ...bankId }>
+                <option value="-1">Loading...</option>
+              </select>
+            </FieldContainer>
+
+
+            <FieldContainer customClassName="for-controls">
+              <button type="submit">Save Transaction</button>
+            </FieldContainer>
+
+          </form>
+
         </div>
       </div>
     );
+
   }
 
 }
+
+
+function validate(values) {
+
+  let errors = {};
+
+  if(!values.amount) {
+    errors.amount = "Please provide amount";
+  }
+  else if(!(/^[\-+]?\d+\.?\d*$/.test(values.amount))) {
+    errors.amount = "Must be a number";
+  }
+
+  if(!values.bankId || values.bankId == -1) {
+    errors.bankId = "Please specify bank";
+  }
+
+  return errors;
+
+}
+
+export default reduxForm({
+  form: "addTransactionForm",
+  fields: ["amount", "bankId"],
+  validate
+}, null, null)(AddTransaction);
